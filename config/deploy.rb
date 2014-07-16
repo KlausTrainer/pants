7b58ec165fe9af4f510841ecb74948857aa8c1b1
Klaus Trainer <klaus_trainer@posteo.de>
2014-07-16 23:13:14 +0200

    Use 'capistrano/rbenv'

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,16 @@ end
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/home/pants'
 
+set :user, 'pants'
+set :use_sudo, false
+
+set :rbenv_path, "#{fetch(:deploy_to)}/rbenv"
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.1.2'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
 # Default value for :scm is :git
 # set :scm, :git
 
