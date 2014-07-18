4840fee300eb7c16aff2b52254bf51b9befddeb0
Klaus Trainer <klaus_trainer@posteo.de>
2014-07-18 12:45:58 +0200

    Remove unnecessary CSRF protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  #
-  protect_from_forgery with: :null_session
-
   # By default, only respond to HTML requests.
   #
   respond_to :html
